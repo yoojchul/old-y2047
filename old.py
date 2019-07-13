822d04b723a5fe2b316e80d3ec673946f6df4305
yoojchul <yoojchul@empal.com>
2019-07-13 13:06:35 +0900

    Add files via upload

--- old.py
+++ old.py
@@ -1,3 +1,4 @@
+import urllib.request
 import json
 import matplotlib.pyplot as plt
 import numpy as np
@@ -6,8 +7,9 @@ import math
 fig = plt.figure(figsize=(12, 12))
 ax = fig.gca()
 
-with open("c:/Users/rcjcyoo/Downloads/skorea-provinces-2018-geo.json", encoding='utf-8') as json_file:
-    jdata = json_file.read()
+#  json file from https://github.com/southkorea/southkorea-maps
+with urllib.request.urlopen("https://drive.google.com/uc?export=download&id=1D-jdcCUXhb9BmhMBHC9z6Ex4a2hMStYC") as json_file:
+    jdata = json_file.read()     
     geoJSON = json.loads(jdata)
 
 pts=[]
@@ -26,11 +28,12 @@ x = [i for i,j in pts]
 y = [j for i,j in pts]
 ax.plot(x,y)
 
-# sidos = 서울, 부산, 대구, 인천, 광주, 
-#           대전, 울산, 세종, 경기, 강원, 
-#           충북, 충남, 전북, 전남, 경북, 
-#           경남, 제주
+# sidos = seould,    pusan,  daegu,  incheon, kwanju
+#            daejeon, ulsan,   sejong,  kyungki, kangwon
+#            chubuk, chunam, junbuk, junnam, kyungbuk
+#            kyungnam,  jeju
 
+colors = ['gray', 'green']
 old_age = [36.6, 41.0, 39.7, 37.8, 36.6, 36.4, 37.0, 27.8, 35.3, 45.0, 40.6, 40.0, 43.9, 46.8, 45.4, 41.4, 36.6]
 sidos = [[127.2, 37.8], [129.35, 35], [128.5, 36.15], [126.2, 37.5], [126.7, 35],
            [127.2, 36.3], [129.6, 35.5], [127, 36.7], [127.3, 37.25], [128.3, 37.8],
@@ -44,12 +47,11 @@ for s, sido  in enumerate(sidos):
     ratios.append(1.0-old_age[s]/100.0)
     start = 0.25
     for i, ratio in enumerate(ratios):
-        x = np.add(np.multiply(np.cos(np.linspace(2*math.pi*start,2*math.pi*(start+ratio), 30)), 0.25), cx)
+        x =  np.cos(np.linspace(2*math.pi*start,2*math.pi*(start+ratio), 30)) *  0.25 + cx
         x1 = [cx] + x.tolist()
-        y = np.add(np.multiply(np.sin(np.linspace(2*math.pi*start,2*math.pi*(start+ratio), 30)), 0.2), cy)
+        y =  np.sin(np.linspace(2*math.pi*start,2*math.pi*(start+ratio), 30)) * 0.2  +  cy
         y1 = [cy] + y.tolist()
         ax.fill(x1,y1, color=colors[i], facecolor=colors[i], alpha=1.0)
         start += ratio
         
 plt.show()
-#plt.savefig("old_age.png")
